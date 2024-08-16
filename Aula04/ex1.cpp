681129af63e6428b35464bd6ac60175ece51f2ac
MatheusCastellucci <matheusc1@al.insper.edu.br>
2024-08-16 13:43:27 -0400

    aula4

---
+++ Aula04/ex1.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // Inclui a biblioteca para entrada e saída de dados
+
+// mains do C++ sempre precisa ser um int, logo sempre precisa retornar algum INT
+int main() {
+    std::cout << "Olá, Mundo!" << std::endl; // Exibe a mensagem na tela    (endl serve para quebrar linha)
+    return 0; // Indica que o programa terminou com sucesso
+}
+
+
+// Comando para compilar: g++ ex1.cpp -o ex1 -> ./ex1 
+
+// outro jeito de fazer
+/*
+using namespace std;
+
+int main ()
+{
+  cout << "Olá mundo!"; 
+  return 0;
+}
+*/
