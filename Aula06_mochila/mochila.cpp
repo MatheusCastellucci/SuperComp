cc2910de13aca239f845760b0f629df7f44425ea
MatheusCastellucci <matheusc1@al.insper.edu.br>
2024-08-28 14:22:22 -0400

    aula06 feita

--- Aula06_mochila/mochila.cpp
+++ Aula06_mochila/mochila.cpp
@@ -26,6 +26,26 @@ int mochilaRecursiva(int capacidade, vector<Item>& itens, int n) {
                mochilaRecursiva(capacidade, itens, n-1));
 }
 
+// Função recursiva para calcular o peso correspondente ao valor máximo
+int pesoMochila(int capacidade, vector<Item>& itens, int n) {
+    // Caso base: sem itens ou capacidade zero
+    if (n == 0 || capacidade == 0)
+        return 0;
+
+    if (itens[n-1].peso > capacidade)
+        return pesoMochila(capacidade, itens, n-1);
+
+    int incluirPeso = itens[n-1].peso + pesoMochila(capacidade - itens[n-1].peso, itens, n-1);
+    int naoIncluirPeso = pesoMochila(capacidade, itens, n-1);
+
+    if (itens[n-1].valor + mochilaRecursiva(capacidade - itens[n-1].peso, itens, n-1) >
+        mochilaRecursiva(capacidade, itens, n-1)) {
+        return incluirPeso;
+    } else {
+        return naoIncluirPeso;
+    }
+}
+
 // Função para ler os dados de entrada de um arquivo
 void lerEntrada(const string& nomeArquivo, int& capacidade, vector<Item>& itens) {
     ifstream arquivo(nomeArquivo);
@@ -49,15 +69,17 @@ int main() {
         int capacidade;
         vector<Item> itens;
 
-        lerEntrada("/mnt/data/" + nomeArquivo, capacidade, itens);
+        lerEntrada(nomeArquivo, capacidade, itens);
 
         auto inicio = chrono::high_resolution_clock::now();
         int valorMaximo = mochilaRecursiva(capacidade, itens, itens.size());
+        int pesoOcupado = pesoMochila(capacidade, itens, itens.size());
         auto fim = chrono::high_resolution_clock::now();
         chrono::duration<double> duracao = fim - inicio;
 
         cout << "Arquivo: " << nomeArquivo << endl;
-        cout << "Valor máximo na mochila: " << valorMaximo << endl;
+        cout << "Peso ocupado: " << pesoOcupado << " Kg" << endl;
+        cout << "Valor máximo na mochila: " << valorMaximo << " dinheiros" << endl;
         cout << "Tempo de execução: " << duracao.count() << " segundos" << endl;
         cout << "---------------------------" << endl;
     }
